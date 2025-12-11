9fd07def0d8fa20bfe7c78b04f3b68db77e2bc2e
Mariya-Vl <mariya.s.vlasova@gmail.com>
2025-12-11 09:39:17 +0200

    Create index page, style, connection, env, htaccess, gitignor

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Taitteti - login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Hello taitteri</h1>
+</body>
+</html>
